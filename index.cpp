295643b54478376d5f20a44bcf20d4e908a50389
vivekkumar0412 <www.vivekkumar0412@gmail.com>
2024-12-15 14:27:49 +0530

    sum of numbers

--- index.cpp
+++ index.cpp
@@ -10,8 +10,20 @@ int convertBinary(int num){
         pow *= 10;
     };
     return ans;
-}
+};
+
+
+int sumOfNumber (int num){
+    int ans = 0;
+    while(num > 0){
+        int last = num % 10;
+        ans += last;
+        num/= 10;
+    };
+    return ans;
+};
 int main() {
     cout<<convertBinary(10)<<endl;
+    cout<<sumOfNumber(1089908)<<endl;
     return 0;
 }
