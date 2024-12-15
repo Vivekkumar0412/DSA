b6b0869df397133b70afb487c9b3967a7c011a48
vivekkumar0412 <www.vivekkumar0412@gmail.com>
2024-12-15 22:17:43 +0530

    left shift and right shift

--- index.cpp
+++ index.cpp
@@ -1,29 +1,37 @@
 #include <iostream>
 using namespace std;
-int convertBinary(int num){
+int convertBinary(int num)
+{
     int ans = 0;
     int pow = 1;
-    while(num > 0){
+    while (num > 0)
+    {
         int last = num % 2;
         ans = ans + (last * pow);
-        num/= 2;
+        num /= 2;
         pow *= 10;
     };
     return ans;
 };
 
-
-int sumOfNumber (int num){
+int sumOfNumber(int num)
+{
     int ans = 0;
-    while(num > 0){
+    while (num > 0)
+    {
         int last = num % 10;
         ans += last;
-        num/= 10;
+        num /= 10;
     };
     return ans;
 };
-int main() {
-    cout<<convertBinary(10)<<endl;
-    cout<<sumOfNumber(1089908)<<endl;
+int main()
+{
+    cout << convertBinary(10) << endl;
+    cout << sumOfNumber(1089908) << endl;
+    cout << (4 & 8) << endl;
+    cout << (4 | 8) << endl;
+    cout << (4 << 4) << endl;
+    cout << (4 >> 4) << endl;
     return 0;
 }
